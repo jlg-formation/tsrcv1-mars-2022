4ba8d214986e578f210217f0ca5a8f9b1e2af5ff
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-10 12:16:49 +0100

    dev: server web ok

--- back/server.js
+++ back/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
+const dir = "./public";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("url", req.url);
+  next();
 });
+
+app.use(express.static(dir));
+app.use(serveIndex(dir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
