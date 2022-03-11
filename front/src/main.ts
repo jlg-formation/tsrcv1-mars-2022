e36b0c920378032b397f1077ce9d85b52306fe80
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-11 09:54:35 +0100

    dev: added small circles

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,33 @@
 import "./style.scss";
 
-console.log("coucou");
+const svgns = "http://www.w3.org/2000/svg";
 
-const truc: number = 123;
-console.log("truc: ", truc);
+const n = 10;
+
+const cx0 = 500;
+const cy0 = 500;
+const r0 = 400;
+
+const drawSmallCircles = () => {
+  const group = document.querySelector("svg g.small-circle");
+
+  for (let i = 0; i < n; i++) {
+    console.log("i: ", i);
+    const circle = document.createElementNS(svgns, "circle");
+    const angle = (i * (2 * Math.PI)) / n;
+    const x = cx0 + r0 * Math.cos(angle);
+    const y = cy0 + r0 * Math.sin(angle);
+    const r = "10";
+    circle.setAttributeNS(null, "cx", x + "");
+    circle.setAttributeNS(null, "cy", y + "");
+    circle.setAttributeNS(null, "r", r);
+    group.appendChild(circle);
+  }
+};
+
+const main = () => {
+  console.log("start");
+  drawSmallCircles();
+};
+
+main();
