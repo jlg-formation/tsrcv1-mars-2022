d9a2fbadfcd4dc31d42746be22e25ee4f21d6824
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-11 09:30:26 +0100

    dev: added webpack dev server

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -37,4 +37,11 @@ module.exports = {
       template: "src/index.html",
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
 };
